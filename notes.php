2601b05a2b3b3b5e19d69cdbaeeb69f10689fe73
John Doe <john.doe@email.com>
2018-10-19 11:48:29 +0200

    Revert "Ajout message dans home.php"
    This reverts commit 2ce6714947316e36e6067d096668666ee071a022.

--- notes.php
+++ notes.php
@@ -27,20 +27,22 @@ install nano editor pas vim.
 
 extra options : default
 
-<h2>Une fois intallé :</h2>
+Une fois intallé :
 
-<pre><code class="language-php line-numbers">git --version</code></pre>
+git --version
 
 <p>C'est une installation globale. Il est dans l'environnement est detecté par PHPStorm.</p>
 
-<h3>on doit initialisé GitHub.</h3>
+<p>on doit initialisé GitHub.</p>
 
-<pre><code class="language-php line-numbers">git init</code></pre>
+git init
 
-<p>il faut configurer git.</p>
+Il faut configurer le config.
+
+il faut configurer git.
 
 <pre><code class="language-php line-numbers">git config --global user.name "John Doe" // Deux variables
-        // Ne doit pas correspond à GitHub obligatoirement mais peut.</code></pre>
+    // Ne doit pas correspond à GitHub obligatoirement mais peut.</code>
 
 <pre><code class="language-php line-numbers">git config --global user.email "john.doe@email.com"
 
@@ -50,12 +52,12 @@ extra options : default
 
     <pre><code class="language-php line-numbers">git add index.php // plusieur commande, mais d'abord indiqué le nom du fichier.</code></pre>
 
-<pre><code class="language-php line-numbers">git status</code></pre>
+    git status
 
-<p>on va créé un ".gitignore"<br>
-    Qui permet d'ajouter les fichiers que l'on ne veus pas commiter.</p>
+    on va créé un ".gitignore"
+    Qui permet d'ajouter les fichiers que l'on ne veus pas commiter.
 
-    Fichier .gitignore commentaire à ajouté par après.
+    fichier .gitignore comment à a jouté par après.
 #
  * Created by PhpStorm.
  * User: nemzag
@@ -113,7 +115,7 @@ C:\Users\nemza\Google Drive\www\gitweb>
 
     C:\Users\nemza\Google Drive\www\gitweb>git log
 commit efb1ac5734caed0614aa5f1288bdd70037e88ffc (HEAD -> master)
-Author: John Doe &lt;john.doe@email.com&gt;
+Author: John Doe <john.doe@email.com>
 Date:   Fri Oct 19 10:21:21 2018 +0200
 
     premier commit projet (files)
@@ -124,7 +126,7 @@ Date:   Fri Oct 19 10:21:21 2018 +0200
 
     C:\Users\nemza\Google Drive\www\gitweb>git log --stat
 commit efb1ac5734caed0614aa5f1288bdd70037e88ffc (HEAD -> master)
-Author: John Doe &lt;john.doe@email.com&gt;
+Author: John Doe <john.doe@email.com>
 Date:   Fri Oct 19 10:21:21 2018 +0200
 
     premier commit projet (files)
@@ -139,15 +141,15 @@ Date:   Fri Oct 19 10:21:21 2018 +0200
     <pre><code class="language-php line-numbers">C:\Users\nemza\Google Drive\www\gitweb>git status
 On branch master
 Changes to be committed:
-  (use "git reset HEAD &lt;file&gt;..." to unstage)
+  (use "git reset HEAD <file>..." to unstage)
 
         new file:   css/main.css
         new file:   css/style.css
         new file:   lire.txt
 
 Changes not staged for commit:
-  (use "git add &lt;file&gt;..." to update what will be committed)
-  (use "git checkout -- &lt;file&gt;..." to discard changes in working directory)
+  (use "git add <file>..." to update what will be committed)
+  (use "git checkout -- <file>..." to discard changes in working directory)
 
         modified:   .gitignore
         modified:   css/main.css
@@ -157,7 +159,7 @@ Changes not staged for commit:
         modified:   todo.md
 
 Untracked files:
-  (use "git add &lt;file&gt;..." to include in what will be committed)
+  (use "git add <file>..." to include in what will be committed)
 
         admin.php
 
@@ -167,7 +169,7 @@ C:\Users\nemza\Google Drive\www\gitweb>git add --all
 C:\Users\nemza\Google Drive\www\gitweb>git status
 On branch master
 Changes to be committed:
-  (use "git reset HEAD &lt;file&gt;..." to unstage)
+  (use "git reset HEAD <file>..." to unstage)
 
         modified:   .gitignore
         new file:   admin.php
@@ -206,58 +208,7 @@ Date:   Fri Oct 19 10:21:21 2018 +0200
         Le commit que je l'ai fait trop rapidement.
 
         <pre><code class="language-php line-numbers">git add --all</code></pre>
-
-        <pre><code class="language-php line-numbers">git commit --amend
-            // La on peut changer le message. Mais on ne le fera pas.
-            // ctrl+x</code></pre>
-        C:\Users\nemza\Google Drive\www\gitweb>git commit --amend
-[master 58dca1a] Ajout des CSS
- Date: Fri Oct 19 11:18:21 2018 +0200
- 7 files changed, 184 insertions(+), 6 deletions(-)
- create mode 100644 admin.php
- create mode 100644 css/main.css
- create mode 100644 css/style.css
- create mode 100644 lire.txt
-
-C:\Users\nemza\Google Drive\www\gitweb>
-
     </code>
-
-    <pre><code class="language-php line-numbers">C:\Users\nemza\Google Drive\www\gitweb>git add --all
-
-C:\Users\nemza\Google Drive\www\gitweb>git status
-On branch master
-Changes to be committed:
-  (use "git reset HEAD <file>..." to unstage)
-
-        modified:   .gitignore
-        new file:   css/grid.css
-        modified:   notes.php
-        modified:   todo.md
-
-
-C:\Users\nemza\Google Drive\www\gitweb></code></pre>
-
-    <pre><code class="language-php line-numbers">C:\Users\nemza\Google Drive\www\gitweb>git rm --cached lire.txt // rm : remove.
-rm 'lire.txt'
-// Voila comment le retirer avant le commit.
-</code></pre>
-
-    <pre><code class="language-php line-numbers">C:\Users\nemza\Google Drive\www\gitweb>git add --all
-
-C:\Users\nemza\Google Drive\www\gitweb>git status
-On branch master
-Changes to be committed:
-  (use "git reset HEAD <file>..." to unstage)
-
-        modified:   .gitignore
-        new file:   css/grid.css
-        modified:   notes.php
-        modified:   todo.md
-
-
-C:\Users\nemza\Google Drive\www\gitweb>
-</code></pre>
 </body>
 </html>
 
